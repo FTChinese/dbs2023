eef3fd4a15d390baa825d5a5c0d9a99eb5027f61
chao.zhu <chao.zhu@corp.ftchinese.com>
2024-04-24 15:55:39 +0800

    pif

---
+++ pif-future-of-tourism/embed.js
@@ -0,0 +1,246 @@
+var nm = `pif-future-of-tourism`;
+var domain = `https://alphagridinfographics.com`;
+var subdomain = domain + `/` + nm + `/`;
+var embededHtml = `<article id="pif-infographic">
+    <div class="sections">
+      <div class="section jarallax background-section video-section in-view" id="section1">
+        <div class="section-video-container">
+          <video playsinline="" webkit-playsinline="" loop="" autoplay="" preload="" muted="" poster="./pif-future-of-tourism/assets/images/saudi-arabia.jpg"><source src="./pif-future-of-tourism/assets/videos/red-palace-header.mp4" type="video/mp4">Your browser does not support the video tag.</video>
+        </div>
+        <div class="section-wrapper">
+          <h1><span>The future</span> <span>of tourism</span></h1>
+          <h2>Redefining the way <br>we travel</h2>
+          <p><span>Saudi Arabia’s blueprint for tourism</span> <span>tomorrow is built on authentic</span><span>and sustainable development today</span></p>
+          <p class="scroll">Scroll</p>
+        </div>
+      </div>
+      <div class="section jarallax" id="section2">
+        <div class="section-wrapper">
+          <div class="column">
+            <p>Dinner in the Red Palace starts at 8pm. Butlers adorned in dark red silks guide guests through the high-vaulted corridors, past giant works of art adorning the walls and into the main dining room.</p>
+            <p>As you take your seat, waiters place vast trays of meat and fish at the centre of the table as the scent of rich spices transport you to another era: the early days of the Saudi nation.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section video-section" id="section3">
+        <div class="section-video-container">
+          <video playsinline="" webkit-playsinline="" loop="" autoplay="" preload="" muted="" poster="./pif-future-of-tourism/assets/images/details.jpg"><source src="./pif-future-of-tourism/assets/videos/red-palace.mp4" type="video/mp4">Your browser does not support the video tag.</video>
+        </div>
+        <div class="section-wrapper">
+          <div class="column">
+            <p class="enhanced quote">It is those details, those moments, that define your experience of a destination,&rdquo;</p>
+            <p>says Mark De Cocinis, CEO of Boutique Group, a hospitality company owned by the Public Investment Fund (PIF) that has been tasked with converting several of Saudi Arabia’s historic palaces for a future generation of tourists.</p>
+            <p>Preserving the country’s rich culture and history is essential to PIF’s plans to diversify the economy, presenting a bold new vision for the future of tourism – and supporting Saudi Arabia’s aim to attract 150mn visitors a year by 2030.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax" id="section4">
+        <div class="section-wrapper">
+          <div class="column">
+            <h2 class="large">This starts with authentic development</h2>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section video-section" id="section5">
+        <div class="section-video-container">
+          <video playsinline="" webkit-playsinline="" loop="" autoplay="" preload="" muted="" poster="./pif-future-of-tourism/assets/images/diriyah.jpg"><source src="./pif-future-of-tourism/assets/videos/mud-bricks.mp4" type="video/mp4">Your browser does not support the video tag.</video>
+        </div>
+        <div class="section-wrapper">
+          <div class="column">
+            <p>About 20km west from the centre of Riyadh, work is well underway on PIF’s ambitious project to turn Diriyah, the historic birthplace of the Kingdom of Saudi Arabia, into one of the world’s key culture and lifestyle destinations. The development is being built entirely in the Najdi architectural style – the distinctive Saudi building aesthetic, set around a courtyard – and will make use of locally sourced materials, including traditionally made mud bricks and limestone. The Diriyah area includes the Unesco World Heritage Site of At-Turaif, and reflects a journey that spans nearly 300 years of Saudi history. Having opened the doors on its first destinations at the end of 2022, Diriyah welcomed 1 mn guests in the first six months of operations.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section" id="section6">
+        <div class="section-wrapper">
+          <img class="jarallax-img" src="./pif-future-of-tourism/assets/images/diriyah1.jpg" alt="Diriyah">
+          <div class="column">
+            <div class="enhanced-wrapper">
+              <p class="enhanced quote">It’s cool that we have Instagram and Snapchat, but technology is only effective if it amplifies the authentic human experience – if it lets you connect with people who want to know where to visit in Diriyah, or what it feels like to run your hand along one of the mud silhouettes.&rdquo;</p>
+              <p><span>Jerry Inzerillo, CEO,</span> <span>Diriyah Company</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax" id="section7">
+        <div class="section-wrapper">
+          <div class="columns">
+            <div class="column">
+              <p>Meanwhile, on the country’s west coast, PIF’s giga-project The Red Sea is welcoming its first guests. The regenerative-tourism destination stretches across 200km of coastline, encompassing more than 90 islands as well as the Al Wajh lagoon, a unique ecological habitat centred around coral reefs, seagrass and mangrove trees.</p>
+              <p>Preserving this natural habitat is a core mission, and Red Sea Global, the firm behind the project, is committed to achieving a 30 per cent net conservation benefit by 2040 at The Red Sea and its sister destination Amaala.</p>
+              <div class="mobile-show">
+                <div class="image-container">
+                  <img src="./pif-future-of-tourism/assets/images/red-sea.jpg" alt="Red sea">
+                  <p class="caption">The Red Sea resort will incorporate 90 unspoiled islands, each home to some of the world’s most unique natural ecosystems</p>
+                </div>
+              </div>
+              <p>But it is also committed to the preservation of Saudi Arabia’s cultural heritage. Red Sea Global is paying for hundreds of local Saudis to study hospitality at some of the country’s best universities, and is employing them across its destinations. &ldquo;Only they can explain local traditions and cultures in an authentic way,&rdquo; says John Pagano, the Group CEO.</p>
+            </div>
+            <div class="column desktop-show">
+              <div class="image-container">
+                <img src="./pif-future-of-tourism/assets/images/red-sea.jpg" alt="Red sea">
+                <p class="caption">The Red Sea resort will incorporate 90 unspoiled islands, each home to some of the world’s most unique natural ecosystems</p>
+              </div>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <p class="enhanced quote">We train them on how to do the guiding, but what we don’t have to train them on is their traditions, the experience of how their parents and grandparents lived on the land; the stories of how their ancestors used the stars to navigate through the desert. People want these authentic experiences and we’re embracing that.&rdquo;</p>
+              <p class="author"><span>John Pagano, CEO,</span> <span>Red Sea Global</span></p>
+              <div class="mobile-show">
+                <div class="image-container">
+                  <img src="./pif-future-of-tourism/assets/images/salwa-palace.jpg" alt="Salwa Palace">
+                  <p class="caption">The mud brick Salwa Palace in the heart of Diriyah, the birthplace of the nation</p>
+                </div>
+              </div>
+              <p>Lars Clasen, CEO of the PIF-owned Cruise Saudi, believes that authenticity is a vital quality in any tourism business, not only in providing an immersive experience for guests but also facilitating operations. &ldquo;Saudi Arabia’s rich culture is what attracts international tourists to its shores, and to date Cruise Saudi has welcomed more than 300,000 passengers representing over 120 nationalities. However, this means that there is a wide variety of different cultures and expectations – with different food preferences, music tastes and even different timings in daily schedules. Offering an experience that is authentically Saudi therefore aligns our passengers in a shared experience.&rdquo;</p>
+            </div>
+            <div class="column desktop-show">
+              <div class="image-container">
+                <img src="./pif-future-of-tourism/assets/images/salwa-palace.jpg" alt="Salwa Palace">
+                <p class="caption">The mud brick Salwa Palace in the heart of Diriyah, the birthplace of the nation</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section" id="section8">
+        <div class="section-wrapper">
+          <img class="jarallax-img" src="./pif-future-of-tourism/assets/images/underwater.jpg" alt="Underwater">
+          <div class="column">
+            <p>With the aim of welcoming </p>
+            <h2 class="boxed">
+              <span class="border-top"></span>
+              <span class="border-right"></span>
+              <span class="border-bottom"></span>
+              <span class="border-left"></span>
+              <span class="block"><span class="counter" data-value="1.3">1.3</span>mn guests</span><span class="block">annually</span>
+            </h2>
+            <p>Cruise Saudi is continually expanding its infrastructure to accommodate increasing numbers, with three ports operational, four under development and three more in the pipeline.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax" id="section9">
+        <div class="section-wrapper">
+          <div class="column">
+            <p>In addition, the company’s newly launched AROYA Cruise will reflect a &ldquo;remarkably Arabian&rdquo; experience and is the first cruise line to embrace the preferences of a GCC audience. It aims to provide a distinctive new concept for holidaying and exploring the Red Sea and beyond. Cruise Saudi is also collaborating with the hospitality company Aman Resorts to launch a luxury yacht, &ldquo;Aman at Sea&rdquo;, geared toward ultra-high-net-worth individuals, for launch by 2027.</p>
+            <div class="enhanced-wrapper">
+              <p class="enhanced with-border">But the future of tourism needs us to act as stewards today</p>
+            </div>
+            <p>After the mass commercialisation of air travel in the late 1950s, the future of tourism was a relatively simple equation: the more people that came to visit, the better for the economy. But today, we know this approach has limits.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section" id="section10">
+        <div class="section-wrapper">
+          <img class="jarallax-img" src="./pif-future-of-tourism/assets/images/door.jpg" alt="The door">
+          <div class="column">
+            <p class="enhanced quote">As the number of visitors to Saudi Arabia multiplies, we need to be very careful that we don’t trample over things that are highly unique, or overcome those communities that weren’t designed for that amount of people. You have to have the courage to say: we can’t do that.&rdquo;</p>
+            <p class="author"><span>Jerry Inzerillo, CEO,</span> <span>Diriyah Company</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax" id="section11">
+        <div class="section-wrapper">
+          <div class="column">
+            <p>Technology provides ways to model the impact of tourism on different locations. In the north-west of Saudi Arabia, digital twins and artificial intelligence are being used to determine how tourists can visit the ancient city of AlUla without impacting the historic environment.</p>
+            <p>Preserving Saudi Arabia’s unique natural environment is also key to PIF’s vision, whether it’s the wild mountain landscapes of Al Soudah in the country’s south-west or the coral reefs of the Red Sea.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section video-section" id="section12">
+        <div class="section-video-container">
+          <video playsinline="" webkit-playsinline="" loop="" autoplay="" preload="" muted="" poster="./pif-future-of-tourism/assets/images/desert.jpg"><source src="./pif-future-of-tourism/assets/videos/diriyah.mp4" type="video/mp4">Your browser does not support the video tag.</video>
+        </div>
+        <div class="section-wrapper">
+          <div class="column">
+            <p class="enhanced">Guests on Cruise Saudi will have access to six UNESCO world heritage sites, including the ancient rock carvings of Mada’in Saleh in Hegra which date back to the first century.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section" id="section13">
+        <div class="section-wrapper">
+          <img class="jarallax-img" src="./pif-future-of-tourism/assets/images/turtle.jpg" alt="Turtle">
+          <div class="column">
+            <p class="enhanced quote">We conducted spatial planning simulations of the 90 islands across our archipelago to find the best location for our resorts. But we decided not to build on our favourite island after we found out it was also the nesting site for the critically endangered hawksbill sea turtle.</p>
+            <p class="enhanced">These ecosystems are incredibly beautiful and incredibly delicate. Red Sea Global used buoys filled with sensors to see if any activity was impacting the natural composition of the water.&rdquo;</p>
+            <p class="author"><span>John Pagano, CEO,</span> <span>Red Sea Global</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax" id="section14">
+        <div class="section-wrapper">
+          <div class="columns">
+            <div class="column">
+              <p>Sustainability is also core to the futuristic visions of tourism underway at NEOM, one of PIF’s five giga-projects. The flagship regions of the development taking shape in north-west Saudi Arabia include a luxury island, Sindalah, and Trojena, NEOM’s year-round mountain destination and the first outdoor ski experience in the region.</p>
+              <p>Epicon, located on the Gulf of Aqaba, is NEOM’s latest luxury coastal tourism destination. Rising from the desert landscape like a glistening mirage, the project will feature two striking towers, one 225 metres tall and one reaching 275 metres, home to an ultra-premium hotel and luxury residences.</p>
+              <p>NEOM tourism destination Leyja, announced in October, is further enriching Saudi Arabia’s ecotourism offering. The development, comprising three hotels, starts from the Gulf of Aqaba coast in the west, then winds inland to form a natural valley carved between 400m-high mountains. Of Leyja’s total area, 95 per cent will be preserved for nature and will combine innovative, ecological design and construction techniques.</p>
+              <div class="mobile-show">
+                <div class="image-container">
+                  <img src="./pif-future-of-tourism/assets/images/leyja.jpg" alt="Leyja">
+                  <p class="caption">One of Leyja’s three hotels in north-west Saudi Arabia, this property will sit prominently at the heart of the wadi’s largest oasis. Its impressive staircase, ascending from the canyon to the entrance of the property, has been designed to offer unrivalled views of the valley beyond</p>
+                </div>
+              </div>
+              <p>This is not sustainability for the sake of it: it’s a strategic necessity to ensure the long-term success of Saudi Arabia’s tourism industry.</p>
+              <p>The future of tourism relies on the actions we make today. &ldquo;Whatever we do, we must make sure we protect this ecosystem,&rdquo; says Pagano. &ldquo;Not only for Saudi Arabia’s own tourism industry, but because pioneering this model can help us lead a shift in the way development and tourism is thought about around the world.&rdquo;</p>
+            </div>
+            <div class="column desktop-show">
+              <div class="image-container">
+                <img src="./pif-future-of-tourism/assets/images/leyja.jpg" alt="Leyja">
+                <p class="caption">One of Leyja’s three hotels in north-west Saudi Arabia, this property will sit prominently at the heart of the wadi’s largest oasis. Its impressive staircase, ascending from the canyon to the entrance of the property, has been designed to offer unrivalled views of the valley beyond</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section jarallax background-section" id="section15">
+        <div class="section-wrapper">
+          <img class="jarallax-img" src="./pif-future-of-tourism/assets/images/leyja1.jpg" alt="Leyja">
+          <div class="column">
+            <p class="enhanced">Of Leyja’s total area, 95 percent will be preserved for nature and will combine innovative, ecological design and construction techniques.</p>
+          </div>
+        </div>
+      </div>
+      <div class="section" id="section16">
+        <div class="section-wrapper">
+          <div class="cta-container">
+            <a class="cta-link" href="https://www.pif.gov.sa/en/Pages/Homepage.aspx?utm_source=ft&utm_medium=referral&utm_campaign=ft-partnership&utm_content=future-of-tourism" target="_blank" rel="noopener"><span>DISCOVER <strong>MORE</strong></span></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>`
+
+var scripts = [`assets/js/main`];
+var styles = [`assets/css/main`,`assets/css/jarallax.min`];
+
+var ag = document.getElementById(`ag-infographic`);
+if(ag){
+	
+	// ag.style.opacity = 0;
+
+	for (var style of styles) {
+		var lp = document.createElement(`link`);
+		lp.rel=`preload`;
+		lp.as=`style`;
+		lp.href=subdomain+style+`.css`
+
+		var ls = document.createElement(`link`);
+		ls.rel=`stylesheet`;
+		ls.href=subdomain+style+`.css`
+
+		document.head.appendChild(lp);
+		document.head.appendChild(ls);
+	}
+
+	for (var script of scripts) {
+
+		var sc = document.createElement(`script`);
+		sc.src = subdomain+script+`.js`;
+		sc.async = true;
+		document.body.appendChild(sc);
+	}
+
+	ag.innerHTML = embededHtml;
+
+} 
