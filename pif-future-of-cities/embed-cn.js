6c127dd1f4db85e24644ddaa804064c2ac41dd00
chao.zhu <chao.zhu@corp.ftchinese.com>
2024-06-27 15:30:59 +0800

    update a image with english txt by a new image with chinese txt

--- pif-future-of-cities/embed-cn.js
+++ pif-future-of-cities/embed-cn.js
@@ -7,16 +7,16 @@ var embededHtml = `<article id="pif-infographic">
         <div class="section-wrapper">
           <img class="jarallax-img" src="/pif-future-of-cities/assets/images/the-line-header.jpg" alt="The future of cities">
           <h1><span>城市</span> <span>的未来</span></h1>
-          <h2>一种新的生活方式</h2>
-          <p><span>为什么解决气候变化和快速城市化需要对城市设计采取更智能的方法。</span></p>
-          <p class="scroll">Scroll</p>
+          <h2>全新的生活方式</h2>
+          <p><span>为何应对气候变化和快速城市化需要更智能的城市设计方案</span></p>
+          <p class="scroll">滚动</p>
         </div>
       </div>
       <div class="section jarallax background-section" id="section2">
         <div class="section-wrapper">
           <img class="jarallax-img" src="/pif-future-of-cities/assets/images/sky-bridges.jpg" alt="42 sky bridges">
           <div class="column">
-            <p>当你在阿卜杜拉国王金融区（KAFD）的</p>
+            <p>清晨，当你在在阿卜杜拉国王金融区（KAFD）</p>
             <h2 class="boxed">
               <span class="border-top"></span>
               <span class="border-right"></span>
@@ -24,17 +24,17 @@ var embededHtml = `<article id="pif-infographic">
               <span class="border-left"></span>
               <span class="block"><span class="counter" data-value="50">50</span> 座</span> <span class="block">空中桥梁</span>
             </h2>
-            <p>之一上品尝咖啡，观看晨阳在利雅得上空移动时，城市开始充满生机。电梯快速上下运行，将爱好运动的通勤者运送到头顶的跑道上，而喷水器正在浇灌沿着城市高大玻璃幕墙侧面的垂直花园。</p>
+            <p>之一上喝着咖啡，看着晨曦洒在利雅得上空时，从某个角落静静欣赏着这一切。城市渐渐苏醒，开始焕发出勃勃生机。电梯快速地上下运行，将充满活力的通勤者送到上方的跑道。与此同时，喷灌系统为城市高大玻璃立面的垂直花园洒下细细水珠。</p>
           </div>
         </div>
       </div>
       <div class="section jarallax" id="section3">
         <div class="section-wrapper">
           <div class="column">
-            <p>甚至曾伴随你早晨通勤的低沉车流声也已消失。在2040年代，城市给人的感觉不同了。</p>
-            <p>但城市世界是从何时开始改变的？</p>
+            <p>曾经伴随人们早晨通勤的低沉交通噪音也已消失。2040年的城市焕然一新，感受完全不同。</p>
+            <p>城市究竟是从什么时候开始发生改变的呢？</p>
             <div class="enhanced-wrapper">
-              <p class="enhanced with-border">容纳更多人口需要当地与远程解决方案的互动。</p>
+              <p class="enhanced with-border">想要容纳更多人口，需要本地和远程解决方案的有机结合。</p>
             </div>
           </div>
         </div>
@@ -43,23 +43,23 @@ var embededHtml = `<article id="pif-infographic">
         <div class="section-wrapper">
           <img class="jarallax-img" src="/pif-future-of-cities/assets/images/kafd.jpg" alt="City population">
           <div class="column">
-            <p>从2018年到2050年，城市人口预计将增加大约</p>
+            <p>预计在2018年至2050年之间，城市人口将增加约</p>
             <h2 class="boxed">
               <span class="border-top"></span>
               <span class="border-right"></span>
               <span class="border-bottom"></span>
               <span class="border-left"></span>
-              <span class="counter" data-value="2.5">2.5</span> 亿
+              <span class="counter" data-value="25">25</span> 亿
             </h2>
-            <p>要在不让城市拥堵的情况下容纳更多人口，需要对城市设计采取更智能的方法。</p>
+            <p>要容纳更多人口的同时而不使城市变得拥挤，我们需要采用更智慧的城市设计方案。</p>
           </div>
         </div>
       </div>
       <div class="section jarallax" id="section5">
         <div class="section-wrapper">
           <div class="column">
-            <p>“当城市迁移增加时，城市倾向于向外扩张，” PIF旗下的新Murabba开发公司（NMDC）代理首席执行官萨巴·巴拉卡特解释道，该公司负责在利雅得开发一个庞大的市中心区域。“这推动了住宅区从市中心的工作和服务地点进一步远离，随着更多人需要进出，这就产生了拥堵。”</p>
-            <p>为了缓解这种拥堵，PIF正在投资大规模的城市开发，以创建一个人们可以在不用长时间通勤的情况下生活、工作和娱乐的社会——在广阔的KAFD的情况下，从家中出发不超过10分钟。</p>
+            <p>“当城市移民不断增加时，城市往往会向外扩张，”公共投资基金（PIF）旗下的新穆拉巴开发公司（NMDC）代理首席执行官沙巴·巴拉卡特解释道。该公司负责在利雅得开发一个庞大的市中心区域。“这种城市扩张增大了住宅区和市中心的工作和服务设施的距离，导致更多人需要长距离通勤，从而造成交通拥堵。”</p>
+            <p>为了缓解这种拥堵，PIF正在投资大规模的城市开发项目，旨在创建方便人们生活、工作和娱乐的社区——以广阔的阿卜杜拉国王金融区（KAFD）为例，人们从家到达这些设施的路程仅需10分钟左右。</p>
           </div>
         </div>
       </div>
@@ -69,26 +69,26 @@ var embededHtml = `<article id="pif-infographic">
         </div>
         <div class="section-wrapper">
           <div class="column">
-          <p>在利雅得未来派的市中心开发区新Murabba中，住宅区不会按照街区进行规划，而是围绕“移动环路”建设——这些自行车道或人行道让你可以步行带孩子去学校，或者骑电动自行车在不到15分钟内到达电影院。</p>
+          <p>在利雅得未来主义的新穆拉巴市中心开发项目中，住宅区不再以传统街区形式布局，而是与“移动环路”毗邻而居。这些环路包括自行车道或步行道，使人们仅需步行送孩子上学或骑电动自行车15分钟内就可以抵达电影院。</p>
           </div>
         </div>
       </div>
       <div class="section jarallax" id="section7">
         <div class="section-wrapper">
           <div class="column">
-            <p>减少大城市中心的瓶颈问题是另一个挑战。解决方案是从三维空间思考。“在KAFD，我们正在建造50座空中桥梁，这不仅更好地利用了垂直空间，而且帮助人们在一个气候可控的步行城市环境中移动，所有必需的便利设施都在家和工作地点10分钟之内，’ KAFD DMC的首席执行官高塔姆·萨希塔尔解释说。</p>
+            <p>减少大城市核心区域的拥堵是一项重大挑战。解决方案在于从三维立体层面进行思考。阿卜杜拉国王金融区开发管理公司（KAFD DMC）首席执行官高塔姆·萨希塔尔解释道：“在阿卜杜拉国王金融区，我们正在建设50座空中桥梁，这不仅能更好地利用空间，还能帮助人们在城市中自由穿行，使所有生活必须的设施都能遍布在其居所或工作地点附件，仅需10分钟就可以抵达。”</p>
             <div class="mobile-show">
               <div class="image-container">
                 <img src="/pif-future-of-cities/assets/images/kafd1.jpg" alt="the KAFD">
                 <p class="caption">到2030年，KAFD将拥有约6100间五星级酒店客房和品牌公寓。</p>
               </div>
             </div>
-            <p>这些以人为本的解决方案是PIF计划帮助利雅得在2030年前进入世界前十大城市经济体的关键，但它们也在开发一个更长远的城市生活模式。</p>
+            <p>这些以人为本的解决方案是公共投资基金（PIF）计划推动利雅得在2030年跻身全球十大城市经济体的关键。同时，它们也为未来的城市生活模式奠定了坚实基础。</p>
           </div>
           <div class="column desktop-show">
             <div class="image-container">
               <img src="/pif-future-of-cities/assets/images/kafd1.jpg" alt="the KAFD">
-              <p class="caption">到2030年，KAFD将拥有约6100间五星级酒店客房和品牌公寓。</p>
+              <p class="caption">到2030年，KAFD 将拥有约6,100间五星级酒店客房和品牌公寓。</p>
             </div>
           </div>
         </div>
@@ -97,7 +97,7 @@ var embededHtml = `<article id="pif-infographic">
         <div class="section-wrapper">
           <img class="jarallax-img" src="/pif-future-of-cities/assets/images/desert.jpg" alt="Desert">
           <div class="column">
-            <p>NEOM是PIF拥有的一个巨型项目，它正在构想未来150年城市的蓝图。从NEOM的山区延伸至红海，长达</p>
+            <p>由公共投资基金（PIF）拥有的超级项目NEOM正在规划未来150年的城市蓝图。该城市将绵延</p>
             <h2 class="boxed">
               <span class="border-top"></span>
               <span class="border-right"></span>
@@ -105,7 +105,7 @@ var embededHtml = `<article id="pif-infographic">
               <span class="border-left"></span>
               <span class="counter" data-value="170">170</span>公里
             </h2>
-            <p>的THE LINE将成为世界上首个智能城市，利用人工智能和其他技术提升人们的现代生活质量。</p>
+            <p>，穿越沙漠，从NEOM的山脉一直延伸到红海。THE LINE将成为世界上第一个人工智能城市，利用人工智能和其他技术，为人们提供更高品质的现代化生活。</p>
           </div>
         </div>
       </div>
@@ -113,8 +113,8 @@ var embededHtml = `<article id="pif-infographic">
         <div class="section-wrapper">
           <div class="columns">
             <div class="column">
-              <p>与其他城市基础设施项目不同，THE LINE是一个绿地项目，这意味着现实和数字架构可以与人们的生活同步建设。例如，城市的第一批居民将会搬入他们在线上装饰、通过虚拟现实走访、并按照精确规格建造的家中。</p>
-              <p>通过将技术置于城市设计的核心，PIF采纳了全球最佳的规划、实施和创新实践。这不仅提高了人们的生活标准，而且确保了城市生活的可持续性。</p>
+              <p>与其他城市基础设施项目建设不同，THE LINE是一个全新的城市项目。这意味着其现实和数字架构可以与当代人们的生活方式与需求同步建设。举例来说，城市中的首批居民将搬入的是他们自己在线上通过虚拟现实设计、体验并按其详细需求为他们建造的家园。</p>
+              <p>公共投资基金（PIF）通过将技术应用于城市设计的核心要素，创造出采用全球最佳的规划、实施和创新实践。这不仅提升了人们的生活水平，还确保了城市发展的可持续性。</p>
               <div class="enhanced-wrapper">
                 <p class="enhanced with-border">让这些城市可持续发展需要更智能地利用周围的环境。</p>
               </div>
