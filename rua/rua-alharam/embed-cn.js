a56f878e28b0420ef5cb87192bafc3b323ff1500
coyzhu <coy.zhu@gmail.com>
2026-03-09 14:37:22 +0800

    add a new rua chinese page

---
+++ rua/rua-alharam/embed-cn.js
@@ -0,0 +1,311 @@
+var embededHtml = `<div class="section-horizontal main">
+  <div class="horizontal-track">
+    <section class="panel">
+      <div class="hero">
+        <div class="hero__top">
+          <div class="container-lg">
+            <div class="hero-title-line-wrapper">
+              <h1 class="hero__title hero__title-desktop desktop-only">重塑 </h1>
+              <h1 class="hero__title hero__title-tablet-mobile tablet-mobile">重-<br>塑</h1>
+            </div>
+            <div class="hero-desc-line-wrapper">
+              <h4 class="hero__desc">建筑：坚守文化传承，适应现代需求</h4>
+            </div>
+          </div>
+        </div>
+        <div class="hero__footer">
+          <div class="container-lg">
+            <h1 class="hero__title">麦加</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section panel panel-split">
+      <article class="article">
+        <div class="container-md">
+          <div class="row"> 
+            <div class="col-md-10 col-lg-6">
+              <h5>随着圣城麦加不断发展扩建，各界已对城市未来达成新的共识：既满足现代生活需求，同时又守护伊斯兰建筑遗产。</h5>
+            </div>
+          </div>
+          <div class="row row-bottom"> 
+            <div class="col-md-4 col-lg-5 col-reduce-s">
+              <div class="map-section">
+                <div class="map__top">
+                  <div class="map">
+                    <div class="pin"> 
+                      <div class="label">麦加</div>
+                      <div class="stem"></div>
+                      <div class="dot"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="map__footer"> 
+                  <h5>Saudi Arabia</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-8 col-offset-lg-1 col-lg-6 col-reduce">
+              <p>与沙特阿拉伯其他地区一样，麦加在过去几十年也经历了深刻的转型，以支持政府旨在实现经济多元化、社会现代化的“2030 愿景”(Vision 2030)。随着朝觐人数持续增长，城市发展的步伐亦同步加速。根据 <a href="https://www.vision2030.gov.sa/en/explore/programs/pilgrim-experience-program" target="_blank">“朝圣者体验计划”(Pilgrim Experience Program)</a>，到 2030 年，沙特每年将接待 3,000 万人次副朝朝觐者，这对城市的基础设施、居住空间和公共空间提出了巨大挑战。</p>
+              <p>在此背景下，麦加将迎来最具深远意义的变革——建设“萨勒曼国王之门”(King Salman Gate)项目。该大型综合项目于 2025 年公布，毗邻大清真寺，总建筑面积达 1,200 万平方米，由沙特公共投资基金 (PIF) 旗下公司 RUA AlHaram AlMakki 牵头开发。项目规划包含约 5 万套住宅单元、1.6 万间酒店客房、约 20 万平方米的零售空间以及超 90 万个新增朝拜空间——相当于大清真寺现有规模的 60%，为这一全球最热门的宗教圣地树立了全新的城市尺度。</p>
+            </div>
+          </div>
+        </div>
+      </article>
+      <hr style="padding-top: 56px">
+    </section>
+    <section class="section panel panel-split">
+      <article class="article">
+        <div class="container-md">
+          <div class="row"> 
+            <div class="col-md-6 col-lg-6">
+              <div class="element"><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i>
+              </div>
+              <h2 class="mb">现代化进程中的<span class="no-break">身份溯源</span></h2>
+            </div>
+          </div>
+          <div class="row row-bottom"> 
+            <div class="col-offset-md-5 col-md-7 col-offset-lg-6 col-lg-6">
+              <p>二十余年来，海外建筑事务所一直活跃于此，已有众多国际机构深度参与当地建设。福斯特建筑事务所 (Foster + Partners) 合伙人 Tony Miki 曾参与设计哈拉曼高铁站及利雅得萨勒曼国王国际机场，亲眼目睹了这座城市的变迁。他回忆道：“2010 年我初到沙特工作时，多数新建项目仍参考西方建筑造型，但现在，我们的设计已植根于当地传统与本土文化。”</p>
+              <p>为防止文化遗产遭受侵蚀与消亡，沙特政府已出台新的指导方针，以加强对历史风貌的保护。2025 年，《萨勒曼国王建筑与城市规划宪章》(King Salman Charter for Architecture and Urbanism) 正式发布了“沙特建筑特色图谱计划”(Saudi Architecture Characters Map)，对 19 种地域性建筑风格进行系统分类并作出规范。每种风格提供传统、过渡和当代三种设计类型，在鼓励创意表达的同时守护古建文脉，包括吉达的珊瑚石建筑和内志的泥砖堡垒。</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel section-img-pattern">
+      <div class="img-card image-reveal img-card--pattern">
+        <div class="img-card__pattern"><img class="mobile-only" src="/rua/rua-alharam/assets/images/1/img-1-pattern-xs.jpg"><img class="tablet-only" src="/rua/rua-alharam/assets/images/1/img-1-pattern-sm.jpg"><img class="desktop-only" src="/rua/rua-alharam/assets/images/1/img-1-pattern.svg"></div>
+        <div class="img-card__img">
+          <div class="mobile-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/quote-1-xs.jpg" data-src="/rua/rua-alharam/assets/images/1/quote-1-xs.jpg" alt="image" title="image"></div>
+          <div class="tablet-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/quote-1-sm.jpg" data-src="/rua/rua-alharam/assets/images/1/quote-1-sm.jpg" alt="image" title="image"></div>
+          <div class="desktop-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/quote-1.jpg" data-src="/rua/rua-alharam/assets/images/1/quote-1.jpg" alt="image" title="image"></div>
+        </div>
+      </div>
+    </section>
+    <section class="panel panel-alt section-quote-article">
+      <div class="quote">
+        <div class="quote__content">
+          <h2 class="quote__title">“The essence of Hejazi architecture is permeability of light, of air, of social connection”</h2>
+          <h6 class="quote__footer">ANDREA SCHIATTARELLA, <span class='no-break'>CHIEF EXECUTIVE,</span> SCHIATTARELLA ASSOCIATI</h6>
+        </div>
+      </div>
+      <article class="article">
+        <div class="container-md">
+          <div class="row row-bottom">
+            <div class="col-md-12">
+              <p>这些指导方针正影响着“萨勒曼国王之门”等大规模项目的设计，确保新建筑满足现代需求，同时尊重地域特色。项目区域内的公寓住宅、酒店塔楼与零售中心的设计规划均遵循这一原则，包括采用遮阳策略、自然通风设计以及与历史遗迹的融合，还将修复多达 1.9 万平方米的历史建筑。</p>
+              <p>国际建筑设计公司 Schiattarella Associati 已深耕沙特 15 年，负责迪里耶艺术未来中心和利雅得乌拉巴公园的设计工作。其首席执行官 Andrea Schiattarella 表示：“对本土文化基因的重新重视表明一个越来越清晰的共识正在形成：未来不在他处，而应向内探寻，新的指导方针通过构筑文化范式调和身份认同与现代创新，回应日益增长的需求。”</p>
+            </div>
+          </div>
+        </div>
+      </article>
+      <hr>
+    </section>
+    <section class="section panel panel-split">
+      <article class="article">
+        <div class="container-md">
+          <div class="row"> 
+            <div class="col-md-7 col-lg-6">
+              <div class="element"> <i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i>
+              </div>
+              <h2 class="mb">探寻麦加的建筑之魂 <span class="no-break">for identity</span></h2>
+            </div>
+          </div>
+          <div class="row row-bottom">
+            <div class="col-offset-md-5 col-md-7 col-offset-lg-6 col-lg-6">
+              <p>麦加率先落实这一倡议，其建筑以希贾兹 (Hejazi) 风格为主导，融汇多元风格，反映了其作为伊斯兰教圣地的崇高威望。西海岸地区的乡土建筑以罗尚窗 (roshan)、庭院及露天屋顶 (kharijah)著称，其中，罗尚窗尤为独特：高大的多层凸窗饰以精美雅致的马什拉比亚 (mashrabiya)木质格栅，其设计初衷是为适应当地的气候环境与文化取向：格栅结构既能促进空气流通，又可遮挡烈日，在沙漠气候中保持室内凉爽。除实用功能外，罗尚窗亦满载美学意蕴与象征意义，体现了传统阿拉伯建筑的精妙匠诣、热情好客的民风以及公共与私人生活的交融。</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel panel-details section-architecture">
+      <section class="architecture">
+        <div class="architecture__track">
+          <div class="architecture__lines">
+            <div class="tablet-mobile"><img class="image lazy" src="/rua/rua-alharam/assets/images/1/ar-line-sm.jpg" data-src="/rua/rua-alharam/assets/images/1/ar-line-sm.jpg" alt="Makkah’s architecture reflects its role at the heart of Islam" title="Makkah’s architecture reflects its role at the heart of Islam"></div>
+            <div class="desktop-only"><img class="image lazy" src="/rua/rua-alharam/assets/images/1/ar-line.jpg" data-src="/rua/rua-alharam/assets/images/1/ar-line.jpg" alt="Makkah’s architecture reflects its role at the heart of Islam" title="Makkah’s architecture reflects its role at the heart of Islam"></div>
+          </div>
+          <div class="tablet-mobile">
+            <div class="architecture__overlay"><img class="img" src="/rua/rua-alharam/assets/images/1/ar-overlay-sm.png"></div>
+          </div>
+          <div class="desktop-only">
+            <div class="architecture__overlay"><img class="img" src="/rua/rua-alharam/assets/images/1/ar-overlay.png"></div>
+          </div>
+          <div class="architecture__images">
+            <div class="tablet-mobile">
+              <div class="img--1"><img class="img" src="/rua/rua-alharam/assets/images/1/ar-img-sm.png"></div>
+            </div>
+            <div class="desktop-only">
+              <div class="img--1"><img class="img" src="/rua/rua-alharam/assets/images/1/ar-img.png"></div>
+            </div>
+          </div>
+          <div class="architecture__text">
+            <p>Makkah’s architecture reflects its role at the heart of Islam</p>
+          </div>
+        </div>
+      </section>
+    </section>
+    <section class="section section-half-article panel panel-txt">
+      <article class="article">
+        <div class="container-md">
+          <div class="row row-bottom">
+            <div class="col-md-12">
+              <p>这些理念亦贯穿于当代项目的建设过程，包括“萨勒曼国王之门”项目的部分住宅与酒店。该项目包含品牌住宅、高端住宅及奢华公寓，并配套 4 至 5 星级酒店，但其在设计上仍强调被动式降温、人性化的街道网络及带遮荫的户外连廊，于现代都市语境重新诠释希贾兹建筑原则。</p>
+              <p>建筑师与设计师的挑战在于如何运用可持续的建造实践，将这些理念融入现代建筑之中。Schiattarella 提醒道：“将其简化为风格过于片面，希贾兹建筑的精髓在于光线通透、空气流通与社会连接。我们的目标并非复制罗尚窗，而是理解其背后的设计逻辑。”</p>
+              <p>福斯特建筑事务所操刀的 2025 年大阪世博会沙特馆荣获金奖，该场馆规划也遵循了相同的思路，其建筑灵感源自传统村落的有机形态，而非盲目堆砌视觉元素。Miki 表示：“我们因变制宜，既咨询专家，也聆听未来居住者的声音——他们掌握着最终的话语权。”</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel panel-img section-img-card">
+      <div class="img-card image-reveal">
+        <div class="img-card__img">
+          <div class="mobile-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/img-3-xs.jpg" data-src="/rua/rua-alharam/assets/images/1/img-3-xs.jpg" alt="Saudi Pavilion, &lt;br&gt;Osaka Expo 2025" title="Saudi Pavilion, &lt;br&gt;Osaka Expo 2025"></div>
+          <div class="tablet-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/img-3-sm.jpg" data-src="/rua/rua-alharam/assets/images/1/img-3-sm.jpg" alt="Saudi Pavilion, &lt;br&gt;Osaka Expo 2025" title="Saudi Pavilion, &lt;br&gt;Osaka Expo 2025"></div>
+          <div class="desktop-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/img-3.jpg" data-src="/rua/rua-alharam/assets/images/1/img-3.jpg" alt="Saudi Pavilion, &lt;br&gt;Osaka Expo 2025" title="Saudi Pavilion, &lt;br&gt;Osaka Expo 2025"></div>
+        </div>
+        <div class="img-card__desc">
+          <h6>Saudi Pavilion, <br>Osaka Expo 2025</h6>
+        </div>
+      </div>
+    </section>
+    <section class="section panel panel-split">
+      <article class="article">
+        <div class="container-md">
+          <div class="row">
+            <div class="col-md-8 col-lg-6">
+              <div class="element"> <i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i>
+              </div>
+              <h2 class="mb">融古汇今，势在必行</h2>
+            </div>
+          </div>
+          <div class="row row-bottom"> 
+            <div class="col-offset-md-4 col-md-8 col-offset-lg-6 col-lg-6">
+              <p>鉴于众多古建因大清真寺扩建而被拆除，新建项目应致力于复兴传统还是追求新意？Schiattarella 从麦加市民和朝觐者的视角看待这个问题，他表示：“传统建筑的核心特征与现代性并不冲突，问题的关键不在于照搬某种建筑语言，而在于理解其设计逻辑，即与气候、材料及社区日常生活的关系。从这个意义上说，建筑师需要的是人文主义视野，而不仅仅是技术理论。”</p>
+              <p>除最新的指导方针外，建筑师还需响应政府的“现代建筑倡议”(Modern Building Initiative)，推广现代建造方法与施工实践，同时兼顾可持续发展。麦加易受气候变化影响，近年来已多次遭遇高温热浪、强沙尘暴、特大洪水等灾害。</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel section-img-pattern">
+      <div class="img-card image-reveal img-card--pattern">
+        <div class="img-card__pattern"><img class="mobile-only" src="/rua/rua-alharam/assets/images/1/img-1-pattern-xs.jpg"><img class="tablet-only" src="/rua/rua-alharam/assets/images/1/img-1-pattern-sm.jpg"><img class="desktop-only" src="/rua/rua-alharam/assets/images/1/img-1-pattern.svg"></div>
+        <div class="img-card__img">
+          <div class="mobile-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/quote-2-xs.jpg" data-src="/rua/rua-alharam/assets/images/1/quote-2-xs.jpg" alt="image" title="image"></div>
+          <div class="tablet-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/quote-2-sm.jpg" data-src="/rua/rua-alharam/assets/images/1/quote-2-sm.jpg" alt="image" title="image"></div>
+          <div class="desktop-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/quote-2.jpg" data-src="/rua/rua-alharam/assets/images/1/quote-2.jpg" alt="image" title="image"></div>
+        </div>
+      </div>
+    </section>
+    <section class="panel panel-alt section-quote-article">
+      <div class="quote">
+        <div class="quote__content">
+          <h2 class="quote__title">“Architects need a humanistic, not merely technical, vision of the city”</h2>
+          <h6 class="quote__footer">ANDREA SCHIATTARELLA, CHIEF EXECUTIVE, SCHIATTARELLA ASSOCIATI</h6>
+        </div>
+      </div>
+      <article class="article">
+        <div class="container-md">
+          <div class="row row-bottom"> 
+            <div class="col-md-12">
+              <p>作为最新公布的开发项目，“萨勒曼国王之门”的规划明确强调适应性与可持续资源管理。</p>
+              <p>项目计划于 2036 年竣工，届时将通过公共交通系统连接住宅、酒店和商业单元。据开发商估算，该项目将创造 30 万个就业岗位，并为沙特 GDP 贡献 4,800 亿里亚尔。但规模与速度并非衡量成功的唯一标准——保护城市建筑遗产不应简化为罗列特征清单，而是要求建筑师与规划者在过去、现在与未来的实用性之间审慎探寻平衡之路。</p>
+            </div>
+          </div>
+        </div>
+      </article>
+      <hr>
+    </section>
+    <section class="section panel panel-split">
+      <article class="article">
+        <div class="container-md">
+          <div class="row"> 
+            <div class="col-md-6 col-lg-5">
+              <div class="element"> <i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i><i class="icon"><img src="/rua/rua-alharam/assets/images/1/icon_rua.svg"></i>
+              </div>
+              <h2 class="mb">致敬历史，筑梦未来</h2>
+            </div>
+          </div>
+          <div class="row row-bottom"> 
+            <div class="col-offset-md-6 col-md-6">
+              <p>麦加建筑的未来不应拘泥于简单复刻传统符号或生硬套用设计公式。城市的规划者和建筑师须在多重约束中寻求最优的平衡点：既要尊崇这片土地深厚的精神底蕴，也要满足居民与朝觐者的现实需求，并为未来的人口增长做好准备。</p>
+              <p>归根结底，建筑唯有赢得社区尊重，才能在岁月的长河中永葆生机，持久焕彩。在这片千百万人前来追寻心灵归宿的土地上，这份责任，便凝结于每一块石材、每一根钢梁、每一道光影之中。</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel section-cta">
+      <div class="img-card image-reveal">
+        <div class="img-card__img">
+          <div class="mobile-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/img-4-xs.jpg" data-src="/rua/rua-alharam/assets/images/1/img-4-xs.jpg"></div>
+          <div class="tablet-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/img-4-sm.jpg" data-src="/rua/rua-alharam/assets/images/1/img-4-sm.jpg"></div>
+          <div class="desktop-only"><img class="lazy" src="/rua/rua-alharam/assets/images/1/img-4.jpg" data-src="/rua/rua-alharam/assets/images/1/img-4.jpg"></div>
+        </div>
+        <div class="img-card__desc">
+          <h6></h6>
+        </div>
+      </div>
+      <div class="cta cta--center">
+        <div class="container-md">
+          <div class="cta__headline"> 
+            <h3 class="cta__title text-center">探索更多关于的信息 <span class="no-break">萨勒曼国王之门</span></h3>
+          </div>
+          <div class="cta__footer"><a class="btn" href="https://www.kingsalmangate.com/?sp=true" target="_blank"><span>了解更多</span></a></div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>`
+
+var styles = [
+  "https://use.typekit.net/pxe5vrq.css",
+  "/rua/rua-alharam/assets/styles/main.css"
+];
+
+var scripts = [`/rua/rua-alharam/assets/scripts/main.js`, `https://www.ft.com/partnercontent/cms/js/main.bundle.js?v=12.0`];
+
+var preconnects = [
+  { href: "https://fonts.googleapis.com", crossorigin: false },
+  { href: "https://fonts.gstatic.com", crossorigin: true }
+];
+
+var ag = document.getElementById('longform');
+if (ag) {
+  for (var preconnect of preconnects) {
+    var linkEl = document.createElement('link');
+    linkEl.rel = 'preconnect';
+    linkEl.href = preconnect.href;
+    if (preconnect.crossorigin) {
+      linkEl.crossOrigin = 'anonymous';
+    }
+    document.head.appendChild(linkEl);
+  }
+
+  for (var style of styles) {
+    var lp = document.createElement('link');
+    lp.rel = 'preload';
+    lp.as = 'style';
+    lp.href = style;
+
+    var ls = document.createElement('link');
+    ls.rel = 'stylesheet';
+    ls.href = style;
+
+    document.head.appendChild(lp);
+    document.head.appendChild(ls);
+  }
+
+  for (var script of scripts) {
+    var sc = document.createElement('script');
+    sc.src = script;
+    sc.async = true;
+    document.body.appendChild(sc);
+  }
+
+  ag.innerHTML = embededHtml;
+}
